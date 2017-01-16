d75b2043beb47640817dc80d342eea0c0ecc0b30
Michael Frederick <master16789@gmail.com>
2017-01-16 15:31:58 -0500

    Created and read testfile

--- LexicalAnalyzer/main.cpp
+++ LexicalAnalyzer/main.cpp
@@ -7,14 +7,28 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-
-using namespace std;
+#include <fstream>
 
 /*
- * 
+ * Main Starting point of the program
  */
 int main(int argc, char** argv) {
-    
+    if (argc > 1) {
+        std::ifstream inputFile(argv[1]);
+        if (inputFile.is_open()) {
+            std::string inputLine;
+            while (getline(inputFile,inputLine)) {
+                std::cout << inputLine << std::endl;
+            }
+            std::cout << "Finished Reading file" << std::endl;
+        }
+        else {
+            std::cout << "Bad Input file" << std::endl;
+        }
+    }
+    else {
+        std::cout << "No Input file given" << std::endl;
+    }
     return 0;
 }
 
