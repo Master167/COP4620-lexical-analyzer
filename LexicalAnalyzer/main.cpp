df012bd942c85bdefc0dccb8e4306aa3ede7fa19
Michael Frederick <master16789@gmail.com>
2017-01-16 15:06:22 -0500

    Netbeans project commit

---
+++ LexicalAnalyzer/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Michael
+ *
+ * Created on January 16, 2017, 2:41 PM
+ */
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
